b5dfaf6cb2cca28845a94a450e02ced3ce22df1d
akur15 <adikurniawanhmzh@gmail.com>
2025-09-17 18:54:30 +0700

    Update login.php

--- login.php
+++ login.php
@@ -1,30 +1,15 @@
 <?php
-$servername = "localhost";
-$dbname = "kasir_app";
-$dbuser = "root"; // sesuaikan
-$dbpass = "";     // sesuaikan
+session_start();
+$username = $_POST['username'];
+$password = $_POST['password'];
 
-$conn = new mysqli($servername, $dbuser, $dbpass, $dbname);
-if ($conn->connect_error) {
-  die("Koneksi gagal: " . $conn->connect_error);
-}
-
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
-  $username = $_POST['username'];
-  $password = $_POST['password'];
-
-  $sql = "SELECT * FROM admin_users WHERE username=? AND password=?";
-  $stmt = $conn->prepare($sql);
-  $stmt->bind_param("ss", $username, $password);
-  $stmt->execute();
-  $result = $stmt->get_result();
-
-  if ($result->num_rows > 0) {
-    // login berhasil
-    header("Location: dashboard.php");
-    exit();
-  } else {
-    echo "<script>alert('Username atau Password salah!'); window.location='index.html';</script>";
-  }
+if ($username == "admin" && $password == "admin123") {
+    header("Location: dashboard_admin.php");
+    exit;
+} else if ($username == "kasir" && $password == "kasir123") {
+    header("Location: dashboard_kasir.php");
+    exit;
+} else {
+    echo "Login gagal!";
 }
 ?>
